e5cfdc49e67799e509e953d5229305a4aa62842c
Aaron Patterson <aaron.patterson@gmail.com>
2013-12-10 17:37:43 -0800

    delete some code

--- lib/magic_scan/reference_image.rb
+++ lib/magic_scan/reference_image.rb
@@ -1,23 +1,29 @@
 require 'magic_scan/database'
 
 module MagicScan
-  class ReferenceCard
+  class Model
     attr_reader :id
 
     def initialize attributes
-      @attributes = attributes
-      @id         = nil
+      @attributes = attributes.each_with_object({}) { |(k,v),o|
+        o[k.to_s] = v
+      }
+      @id            = nil
     end
 
     def init_with_attrs id, attributes
       @id         = id
-      @attributes = attributes
+      @attributes = attributes.each_with_object({}) { |(k,v),o|
+        o[k.to_s] = v
+      }
     end
 
     def method_missing method, *args, &block
-      @attributes.fetch(method) { super }
+      @attributes.fetch(method.to_s) { super }
     end
+  end
 
+  class ReferenceCard < Model
     def save!
       sql = "INSERT INTO reference_cards #{@attributes.keys.sort.join(", ")}"
         " VALUES (#{@attributes.values.map { "?" }.join ", "})"
@@ -28,9 +34,7 @@ module MagicScan
     end
   end
 
-  class ReferenceImage
-    attr_reader :mvid, :filename, :id
-
+  class ReferenceImage < Model
     def self.create mvid, hash, filename
       right = hash & 0xFFFFFFFF
       left  = (hash >> 32) & 0xFFFFFFFF
@@ -78,20 +82,6 @@ module MagicScan
       find_by_id row.first
     end
 
-    def initialize attributes
-      @attributes = attributes.each_with_object({}) { |(k,v),o|
-        o[k.to_s] = v
-      }
-      @id            = nil
-    end
-
-    def init_with_attrs id, attributes
-      @id         = id
-      @attributes = attributes.each_with_object({}) { |(k,v),o|
-        o[k.to_s] = v
-      }
-    end
-
     def fingerprint
       (fingerprint_l << 32) + fingerprint_r
     end
@@ -103,12 +93,6 @@ module MagicScan
       Database.exec sql, @attributes.keys.sort.map { |k| @attributes[k] }
       @id = Database.connection.last_insert_row_id
       self
-    end
-
-    private
-
-    def method_missing method, *args, &block
-      @attributes.fetch(method.to_s) { super }
     end
   end
 end
