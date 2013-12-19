a17ad298948584bfda450e5c2deb47c6a77b382a
Aaron Patterson <aaron.patterson@gmail.com>
2013-12-18 17:43:18 -0800

    add the user image

---
+++ app/models/user_image.rb
@@ -0,0 +1,22 @@
+require 'image'
+require 'tempfile'
+
+class UserImage < Image
+  def self.build_from_bytes bytes
+    tf = Tempfile.open self.name
+    tf.binmode
+    tf.write bytes
+    tf.flush
+    fingerprint = Phashion.image_hash_for tf.path
+    tf.close
+    tf.unlink
+
+    digest = Digest::MD5.hexdigest bytes
+    dest_dir = File.join 'app', 'assets', 'images', digest[0,2]
+    dest_file = File.join dest_dir, "#{digest}.jpg"
+    File.open(dest_file, 'wb') { |f| f.write bytes }
+
+    build(:fingerprint => fingerprint,
+           :filename    => dest_file)
+  end
+end
