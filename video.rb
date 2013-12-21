d007bddaee145519a62267fb742f287862d93593
Aaron Patterson <aaron.patterson@gmail.com>
2013-12-21 14:27:02 -0800

    refactored a little

--- video.rb
+++ video.rb
@@ -11,7 +11,7 @@ win.resize 233, 310
 class FindAndCrop
   attr_reader :width, :height, :to
 
-  def initialize frames, width, height
+  def initialize width, height
     @to = [
       OpenCV::CvPoint2D32f.new(0, 0),
       OpenCV::CvPoint2D32f.new(width, 0),
@@ -20,29 +20,28 @@ class FindAndCrop
     ]
     @width = width
     @height = height
-    @frames = frames
   end
 
-  def each
-    @frames.each do |img|
-      strategy = MagicScan::Contours::Simple.new img
-      from = strategy.corners
+  def process img
+    strategy = MagicScan::Contours::Simple.new img
+    from = strategy.corners
 
-      unless from.empty?
-        transform = OpenCV::CvMat.get_perspective_transform(from, to)
-        new_img = img.warp_perspective transform
-        new_img.set_roi OpenCV::CvRect.new(0, 0, width, height)
-        yield new_img
-      end
+    unless from.empty?
+      transform = OpenCV::CvMat.get_perspective_transform(from, to)
+      new_img = img.warp_perspective transform
+      new_img.set_roi OpenCV::CvRect.new(0, 0, width, height)
+      yield new_img
     end
   end
 end
 
 frames = MagicScan::Frames.new dev
-frames = FindAndCrop.new frames, 233, 310
+fc = FindAndCrop.new 233, 310
 
 frames.each do |img|
-  win.show_image img
+  fc.process(img) do |cut|
+    win.show_image cut
+  end
   if 113 == OpenCV::GUI.wait_key(10)
     break
   end
